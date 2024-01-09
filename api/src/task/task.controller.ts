3016634f9cba6b061995e8fe303981e5aba77435
elibychkova <eli.bychkova@gmail.com>
2024-01-10 00:06:03 +0300

    add service:  create, findAll, findOne, deleteOne

--- api/src/task/task.controller.ts
+++ api/src/task/task.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskService } from './task.service';
+import { ITask } from './interfaces/user.interface';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post('/create')
+  async create(@Body() createTaskDto: CreateTaskDto): Promise<ITask> {
+    return this.taskService.create(createTaskDto);
+  }
+
+  @Get('/get-tasks')
+  async getAllTasks(): Promise<ITask[]> {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<ITask> {
+    return this.taskService.findOne(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<ITask> {
+    return this.taskService.deleteOne(id);
+  }
+}
--- api/src/task/task.service.ts
+++ api/src/task/task.service.ts
@@ -2,10 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ITask } from './interfaces/user.interface';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
-  constructor(@InjectModel('Task') private readonly taskModel: Model<ITask>) {
-    console.log('taskModel :>> ', taskModel);
+  constructor(@InjectModel('Task') private readonly taskModel: Model<ITask>) {}
+
+  async create(createTaskDto: CreateTaskDto): Promise<ITask> {
+    const createdTask = new this.taskModel(createTaskDto);
+    return createdTask.save();
+  }
+
+  async findAll(): Promise<ITask[]> {
+    return this.taskModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<ITask> {
+    return this.taskModel.findById(id);
+  }
+
+  async deleteOne(id: string): Promise<ITask> {
+    return this.taskModel.findByIdAndDelete(id);
   }
 }
